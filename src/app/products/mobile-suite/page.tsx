560556970fb63018a3f77e70d45f9a24fb1f4987
ftgeorge <ftgeorgean@gmail.com>
2025-08-06 11:53:01 +0100

    bug fix

--- src/app/products/mobile-suite/page.tsx
+++ src/app/products/mobile-suite/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import { Smartphone } from 'lucide-react';
 import SectionContainer from '@/components/ui/sectionContainer';
--- src/app/products/saas-platform/page.tsx
+++ src/app/products/saas-platform/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import { Server } from 'lucide-react';
 import SectionContainer from '@/components/ui/sectionContainer';
